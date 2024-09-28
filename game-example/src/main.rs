d618e208347426c571afb6a81beaf26798c0d906
pickles976 <sebaslogo@gmail.com>
2024-09-28 11:46:50 -0500

    added sprite positioning

--- game-example/src/main.rs
+++ game-example/src/main.rs
@@ -1,15 +1,20 @@
-use sdl2::pixels::Color;
 use sdl2::event::Event;
+use sdl2::image::{self, InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
-use sdl2::render::{WindowCanvas, Texture};
-use sdl2::image::{self, LoadTexture, InitFlag};
+use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
+use sdl2::render::{Texture, WindowCanvas};
 use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture) -> Result<(), String> {
+fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture, position: Point, sprite: Rect) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
+    let (width, height) = canvas.output_size()?;
 
-    canvas.copy(texture, None, None);
+    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+
+    canvas.copy(texture, sprite, screen_rect)?;
     canvas.present();
 
     Ok(())
@@ -21,16 +26,22 @@ fn main() -> Result<(), String> {
 
     let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
-    let window = video_subsystem.window("game tutorial", 800, 600)
+    let window = video_subsystem
+        .window("game tutorial", 800, 600)
         .position_centered()
         .build()
         .expect("Could not initialize video subsystem!");
 
-    let mut canvas = window.into_canvas().build()
+    let mut canvas = window
+        .into_canvas()
+        .build()
         .expect("Could not make a canvas!");
 
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/bardo.png")?;
+
+    let position = Point::new(0,0);
+    let sprite = Rect::new(0,0,26, 36);
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -39,19 +50,20 @@ fn main() -> Result<(), String> {
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
                 _ => {}
             }
         }
-        
+
         // Update
         i = (i + 1) % 255;
 
         // Render
-        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture)?;
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite)?;
 
         // Time Management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
