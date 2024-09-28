5a9209d01dd84fd609e1760548d7d5fec5518a20
pickles976 <sebaslogo@gmail.com>
2024-09-28 12:24:43 -0500

    Added input handling

--- game-example/src/main.rs
+++ game-example/src/main.rs
@@ -1,20 +1,28 @@
 use sdl2::event::Event;
-use sdl2::image::{self, InitFlag, LoadTexture};
 use sdl2::keyboard::Keycode;
+use sdl2::image::{self, InitFlag, LoadTexture};
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 use sdl2::render::{Texture, WindowCanvas};
+use sdl2::sys::KeyCode;
 use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture, position: Point, sprite: Rect) -> Result<(), String> {
+#[derive(Debug)]
+struct Player {
+    position: Point, 
+    sprite: Rect,
+    speed: i32
+}
+
+fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture, player: &Player) -> Result<(), String> {
     canvas.set_draw_color(color);
     canvas.clear();
     let (width, height) = canvas.output_size()?;
 
-    let screen_position = position + Point::new(width as i32 / 2, height as i32 / 2);
-    let screen_rect = Rect::from_center(screen_position, sprite.width(), sprite.height());
+    let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, player.sprite.width(), player.sprite.height());
 
-    canvas.copy(texture, sprite, screen_rect)?;
+    canvas.copy(texture, player.sprite, screen_rect)?;
     canvas.present();
 
     Ok(())
@@ -40,8 +48,11 @@ fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
     let texture = texture_creator.load_texture("assets/bardo.png")?;
 
-    let position = Point::new(0,0);
-    let sprite = Rect::new(0,0,26, 36);
+    let mut player = Player { 
+        position: Point::new(0,0), 
+        sprite: Rect::new(0,0,26,36),
+        speed: 5
+    };
 
     let mut event_pump = sdl_context.event_pump()?;
     let mut i = 0;
@@ -50,11 +61,22 @@ fn main() -> Result<(), String> {
     'running: loop {
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit { .. }
-                | Event::KeyDown {
-                    keycode: Some(Keycode::Escape),
-                    ..
-                } => break 'running,
+                Event::Quit { .. } |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => { 
+                    break 'running; 
+                },
+                Event::KeyDown { keycode: Some(Keycode::Left),  .. } => {
+                    player.position = player.position.offset(-player.speed, 0);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Right),  .. } => {
+                    player.position = player.position.offset(player.speed, 0);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Up),  .. } => {
+                    player.position = player.position.offset(0, -player.speed);
+                },
+                Event::KeyDown { keycode: Some(Keycode::Down),  .. } => {
+                    player.position = player.position.offset(0, player.speed);
+                },
                 _ => {}
             }
         }
@@ -63,7 +85,7 @@ fn main() -> Result<(), String> {
         i = (i + 1) % 255;
 
         // Render
-        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, position, sprite)?;
+        render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player)?;
 
         // Time Management
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
