7ddf5255c08afbb5e16a0d9f050348eeda14c400
pickles976 <sebaslogo@gmail.com>
2024-09-28 14:15:13 -0500

    stuff

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 Follow this guide
 - [ ] https://sunjay.dev/learn-game-dev/getting-started.html
-	- [ ] https://sunjay.dev/learn-game-dev/single-animation.html
+	- [ ] https://sunjay.dev/learn-game-dev/adding-ecs.html
 - [ ] create snake game
 - [ ] create tetris
 - [ ] 3D demo
--- game-example/src/main.rs
+++ game-example/src/main.rs
@@ -22,6 +22,18 @@ struct Player {
     sprite: Rect,
     speed: i32,
     direction: Direction,
+    current_frame: i32,
+}
+
+/// Returns the row of the spritesheet corresponding to the given direction
+fn direction_spritesheet_row(direction: Direction) -> i32 {
+    use self::Direction::*;
+    match direction {
+        Up => 3,
+        Down => 0,
+        Left => 1,
+        Right => 2,
+    }
 }
 
 fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture, player: &Player) -> Result<(), String> {
@@ -29,10 +41,18 @@ fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture, player: &P
     canvas.clear();
     let (width, height) = canvas.output_size()?;
 
+    let (frame_width, frame_height) = player.sprite.size();
+    let current_frame = Rect::new(
+        player.sprite.x() + frame_width as i32 * player.current_frame,
+        player.sprite.y() + frame_height as  i32 * direction_spritesheet_row(player.direction),
+        frame_width,
+        frame_height,
+    );
+
     let screen_position = player.position + Point::new(width as i32 / 2, height as i32 / 2);
     let screen_rect = Rect::from_center(screen_position, player.sprite.width(), player.sprite.height());
 
-    canvas.copy(texture, player.sprite, screen_rect)?;
+    canvas.copy(texture, current_frame, screen_rect)?;
     canvas.present();
 
     Ok(())
@@ -54,6 +74,12 @@ fn update_player(player: &mut Player) {
         Down => {
             player.position = player.position.offset(0, player.speed);
         },
+    }
+
+    // Only continue to animate if the player is moving
+    if player.speed != 0 {
+        // Cheat: using the fact that all animations are 3 frames (NOT extensible)
+        player.current_frame = (player.current_frame + 1) % 3;
     }
 }
 
@@ -82,6 +108,7 @@ fn main() -> Result<(), String> {
         sprite: Rect::new(0,0,26,36),
         speed: 0,
         direction: Direction::Right,
+        current_frame: 0,
     };
 
     let mut event_pump = sdl_context.event_pump()?;
