f1f856137f915fb6a8bc8777da9f39f60be90502
pickles976 <sebaslogo@gmail.com>
2024-09-28 12:35:31 -0500

    Added stuff

---
+++ README.md
@@ -0,0 +1,6 @@
+Follow this guide
+- [ ] https://sunjay.dev/learn-game-dev/getting-started.html
+	- [ ] https://sunjay.dev/learn-game-dev/single-animation.html
+- [ ] create snake game
+- [ ] create tetris
+- [ ] 3D demo
--- game-example/src/main.rs
+++ game-example/src/main.rs
@@ -4,14 +4,24 @@ use sdl2::image::{self, InitFlag, LoadTexture};
 use sdl2::pixels::Color;
 use sdl2::rect::{Point, Rect};
 use sdl2::render::{Texture, WindowCanvas};
-use sdl2::sys::KeyCode;
 use std::time::Duration;
+
+const PLAYER_MOVEMENT_SPEED: i32 = 20;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
 
 #[derive(Debug)]
 struct Player {
     position: Point, 
     sprite: Rect,
-    speed: i32
+    speed: i32,
+    direction: Direction,
 }
 
 fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture, player: &Player) -> Result<(), String> {
@@ -26,6 +36,25 @@ fn render(canvas: &mut WindowCanvas, color: Color, texture: &Texture, player: &P
     canvas.present();
 
     Ok(())
+}
+
+// to be unpredictable!
+fn update_player(player: &mut Player) {
+    use self::Direction::*;
+    match player.direction {
+        Left => {
+            player.position = player.position.offset(-player.speed, 0);
+        },
+        Right => {
+            player.position = player.position.offset(player.speed, 0);
+        },
+        Up => {
+            player.position = player.position.offset(0, -player.speed);
+        },
+        Down => {
+            player.position = player.position.offset(0, player.speed);
+        },
+    }
 }
 
 fn main() -> Result<(), String> {
@@ -51,7 +80,8 @@ fn main() -> Result<(), String> {
     let mut player = Player { 
         position: Point::new(0,0), 
         sprite: Rect::new(0,0,26,36),
-        speed: 5
+        speed: 0,
+        direction: Direction::Right,
     };
 
     let mut event_pump = sdl_context.event_pump()?;
@@ -66,16 +96,26 @@ fn main() -> Result<(), String> {
                     break 'running; 
                 },
                 Event::KeyDown { keycode: Some(Keycode::Left),  .. } => {
-                    player.position = player.position.offset(-player.speed, 0);
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction = Direction::Left;
                 },
                 Event::KeyDown { keycode: Some(Keycode::Right),  .. } => {
-                    player.position = player.position.offset(player.speed, 0);
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction = Direction::Right;
                 },
                 Event::KeyDown { keycode: Some(Keycode::Up),  .. } => {
-                    player.position = player.position.offset(0, -player.speed);
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction = Direction::Up;
                 },
                 Event::KeyDown { keycode: Some(Keycode::Down),  .. } => {
-                    player.position = player.position.offset(0, player.speed);
+                    player.speed = PLAYER_MOVEMENT_SPEED;
+                    player.direction = Direction::Down;
+                },
+                Event::KeyUp { keycode: Some(Keycode::Left), repeat: false, .. } |
+                Event::KeyUp { keycode: Some(Keycode::Right), repeat: false, .. } |
+                Event::KeyUp { keycode: Some(Keycode::Up), repeat: false, .. } |
+                Event::KeyUp { keycode: Some(Keycode::Down), repeat: false, .. } => {
+                    player.speed = 0;
                 },
                 _ => {}
             }
@@ -83,12 +123,13 @@ fn main() -> Result<(), String> {
 
         // Update
         i = (i + 1) % 255;
+        update_player(&mut player);
 
         // Render
         render(&mut canvas, Color::RGB(i, 64, 255 - i), &texture, &player)?;
 
         // Time Management
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
     }
     Ok(())
 }
